d265fd2c5dbcbf855d8a5cbd405f4b894f250783
Izuchukwu Samson <81382764+IzuchukwuSamson@users.noreply.github.com>
2022-03-02 20:21:25 +0100

    Another commit

---
+++ client/src/views/Register.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+      <h1>Register</h1>
+
+      <input type="email"
+             name="email"
+      placeholder="email">
+
+      
+      <input type="password"
+             name="password"
+      placeholder="password">
+  </div>
+</template>
+
+
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <div>
+      <h1>Register</h1>
+
+      <input type="email"
+             name="email"
+             v-model="email"
+      placeholder="email">
+
+<br>
+      
+      <input type="password"
+             name="password"
+             v-model="password"
+      placeholder="password">
+
+      <br>
+
+      <button @click="register">Register</button>
+  </div>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+       async register () {
+           const response = await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+            console.log(response.data)
+        }
+    }
+}
+</script>
+
+
+
